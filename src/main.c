3c1235584de6cdef5c661875a6bde359bead27ea
Aaron Beck <abeck@Epona.local>
2023-03-08 21:35:41 -0600

    Commented out sensor initialization and added xbee initialization for now

--- src/main.c
+++ src/main.c
@@ -9,29 +9,28 @@
 #include <asf.h>
 #include <logger.h>
 #include <sensors.h>
+#include "wireless/xbee.h"
 
 int main(void)
 {
     sysclk_init();  // necessary for program startup
     board_init();   // necessary to enable peripherals
+    LED_On(LED0);
 
     logger_init();
+    xbee_init();
 
-    // wrapper to object-like sensors
     //sensor_t trisonica_mini = {tsm_init, tsm_read};
     //sensor_t telaire = {telaire_init, telaire_read};
-    sensor_t coz_ir = {coz_ir_init, coz_ir_read};
+    //sensor_t coz_ir = {coz_ir_init, coz_ir_read};
 
     //trisonica_mini.init();
     //telaire.init();
-    coz_ir.init();
-
-    LOG(DEBUG_LEVEL, "initialized all peripherals, starting main loop.");
+    //coz_ir.init();
 
     while(1)
     {   // main loop
         LED_Toggle(LED0);
-        coz_ir.read();
         delay_ms(500);   
     }
 }
