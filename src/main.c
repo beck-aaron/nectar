41c69c0333a608ba47b18d3974f62ae0b256a4a9
Aaron Beck <abeck@Epona.local>
2023-03-09 21:34:25 -0600

    Removed old comments for cleaner sandbox

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include <asf.h>
 #include <logger.h>
 #include <sensors.h>
+//#include "wireless/codec/encoder.h"
+
 #include "wireless/xbee.h"
 
 int main(void)
@@ -19,14 +21,6 @@ int main(void)
 
     logger_init();
     xbee_init();
-
-    //sensor_t trisonica_mini = {tsm_init, tsm_read};
-    //sensor_t telaire = {telaire_init, telaire_read};
-    //sensor_t coz_ir = {coz_ir_init, coz_ir_read};
-
-    //trisonica_mini.init();
-    //telaire.init();
-    //coz_ir.init();
 
     while(1)
     {   // main loop
