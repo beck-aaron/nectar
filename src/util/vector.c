d28e9a1f636ac3942eacfa48a2bd9144bcbb6ba2
Aaron Beck <abeck@Epona.local>
2023-03-11 16:46:28 -0600

    Updated vector utility to initialize its own memory or tap into existing memory.

--- src/util/vector.c
+++ src/util/vector.c
@@ -1,21 +1,33 @@
 #include "vector.h"
+#include "logger.h"
 
-void v_set(const uint8_t* byte, size_t size)
+vector_t* vector_init(size_t size)
 {
-    vector.data = (uint8_t*)byte;
-    vector.limit = size;
-    vector.size = 0;
+    vector_t* vector = malloc(sizeof(*vector));
+    vector->data = calloc(sizeof(uint8_t), size);
+    vector->size = 0;
+    vector->limit = size;
+    return  vector;
+}
+
+vector_t* vector_set(uint8_t* byte, size_t size)
+{
+    vector_t* vector = malloc(sizeof(*vector));
+    vector->data = byte;
+    vector->size = 0;
+    vector->limit = size;
+    return  vector;
 }
 
 // little endian push
-void v_push(const void* byte, size_t size)
+void vector_push(const void* byte, size_t size, vector_t* vector)
 {
     for (size_t i = size; i > 0; --i)
     {
-        if (vector.size > vector.limit)
-            return;
+        if (vector->size > vector->limit)
+            return; // reallocate data
 
-        vector.data[vector.size++] = ((uint8_t*)byte)[i-1];
+        vector->data[vector->size++] = ((uint8_t*)byte)[i-1];
     }
 }
 
@@ -23,19 +35,30 @@ void v_push(const void* byte, size_t size)
 //only works for byte-sized arrays!!!
 //to make it work for multiple data type arrays, need to define the size of one
 //element and use the prior push function for each element.
-void v_push_bytes(const void* byte, size_t size)
+void vector_push_bytes(const void* byte, size_t size, vector_t* vector)
 {
     for (size_t i = 0; i < size; ++i)
     {
-        if (vector.size > vector.limit)
+        if (vector->size > vector->limit)
             return;
 
-        vector.data[vector.size++] = ((uint8_t*)byte)[i];
+        vector->data[vector->size++] = ((uint8_t*)byte)[i];
     }
 }
 
-void v_clear(void)
+void vector_clear(vector_t* vector)
 {
-    memset(vector.data, 0, vector.size);
-    vector.size = 0;
+    memset(vector->data, 0, vector->size);
+    vector->size = 0;
+}
+
+void vector_print(vector_t* vector)
+{
+    LOG(DEBUG_LEVEL, "vector size: %u", vector->size);
+    LOGHEX(DEBUG_LEVEL, "vector dump", vector->data, vector->limit);
+}
+
+void vector_destroy(vector_t* vector)
+{
+    free(vector);
 }
--- src/util/vector.h
+++ src/util/vector.h
@@ -1,28 +1,27 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-void v_set(const uint8_t* byte, size_t size);
-void v_push(const void* byte, size_t size);
-void v_push_bytes(const void* byte, size_t size);
-void v_clear(void);
-
 typedef struct
 {
-    uint8_t*    data;
-    size_t      size;
-    size_t      limit;
-    void (*set)(const uint8_t* data, size_t size);
-    void (*push)(const void* byte, size_t size);
-    void (*push_bytes)(const void* byte, size_t size);
-    void (*clear)(void);
+    uint8_t* data;
+    size_t size;
+    size_t limit;
+
 } vector_t;
 
-extern vector_t vector;
+// initialize vector with data
+vector_t* vector_init(size_t size);
+vector_t* vector_set(uint8_t* byte, size_t size);
+void vector_push(const void* byte, size_t size, vector_t* vector);
+void vector_push_bytes(const void* byte, size_t size, vector_t* vector);
+void vector_clear(vector_t* vector);
+void vector_print(vector_t* vector);
+void vector_destroy(vector_t* vector);
 
-#define VECTOR() {.set = v_set, .push = v_push, .push_bytes = v_push_bytes, .clear = v_clear};
 
 #endif /* _BYTE_VECTOR_H_ */
